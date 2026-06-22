6a48cf3c256f32bf743ddccc161cb0cfb1e5ea29
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-06-22 15:44:53 +0400

    Preparing The App For Http Requests & Adding a Backend

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,14 @@ import React from 'react'
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 function NewMeetupPage() {
+    function addMeetupHandler(meetupData) {
+        
+    }
+
   return (
     <section>
         <h1>Add New Meetup</h1>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   )
 }
