70e2cd6f2ce1707cafefa4142fb89cad13b7d561
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-06-22 16:26:09 +0400

    Navigating Programmatically

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,11 @@
-import React from 'react'
-import NewMeetupForm from '../components/meetups/NewMeetupForm'
+import { useNavigate } from 'react-router-dom';
+
+import React from 'react';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+    const navigate = useNavigate();
+
     function addMeetupHandler(meetupData) {
         fetch(
             'https://react-getting-started-cec34-default-rtdb.firebaseio.com/meetups.json',
@@ -12,7 +16,9 @@ function NewMeetupPage() {
                     'Content-Type':'application/json'
                 }
             }
-        );
+        ).then(() => {
+            navigate('/', {replace:true});
+        });
     }
 
   return (
